580e11522af8f65bf7b8576242e146e4f728f937
thevoidking <isxca05@gmail.com>
2024-05-30 01:41:38 -0500

    Update shared.lua
    T H E O N E

--- weapon_gblaster_the_one/shared.lua
+++ weapon_gblaster_the_one/shared.lua
@@ -26,8 +26,8 @@ SWEP.WorldModel = "models/weapons/w_physics.mdl"
 SWEP.ShowViewModel = true
 SWEP.ShowWorldModel = true
 
-SWEP.Purpose			= "Vaporize players with a ton of damage"
-SWEP.Instructions		= "M1 to shoot laser."
+SWEP.Purpose			= "you are the one, the one and true, beyond god and his so called creations."
+SWEP.Instructions		= "punish them."
 SWEP.BounceWeaponIcon	= false 
 
 SWEP.UseHands = true
